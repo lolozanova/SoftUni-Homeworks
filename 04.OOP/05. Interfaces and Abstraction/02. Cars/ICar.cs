1d90698a0a46eb86ecaa48c7995aeb451c89a974
Lora Lozanova <lolozanova@gmail.com>
2021-01-02 08:05:26 +0200

    Interfaces and Abstraction

---
+++ 04.OOP/05. Interfaces and Abstraction/02. Cars/ICar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get;  set; }
+        public string  Color { get;  set; }
+
+        public string Start();
+
+        public string Stop();
+
+
+    }
+}
